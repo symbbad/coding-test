5a7122a3841c50a17bce8f9ee7ce0f82c7edfa3f
symbbad <symbbad@gmail.com>
2024-06-08 13:11:47 +0900

    imporved solve-1004

--- baekjoon/1004.py
+++ baekjoon/1004.py
@@ -3,17 +3,23 @@ input = sys.stdin.readline
 
 testcase = int(input())
 
-def between(x, y):
-    return ((x[0]-y[0])**2 + (x[1]-y[1])**2)**(1/2)
+def squared_distance(x, y):
+    return (x[0]-y[0]) ** 2 + (x[1]-y[1]) ** 2
 
 for _ in range(testcase):
     ans = 0
     coordinates = list(map(int, input().split()))
-    planet = int(input())
-    planets = [list(map(int, input().split())) for _ in range(planet)]
-    for i in range(len(planets)):
-        if between(coordinates[0:2], planets[i][0:2]) > planets[i][2] and between(coordinates[2:], planets[i][0:2]) < planets[i][2]:
+    start, end = coordinates[0:2], coordinates[2:]
+    planet_count = int(input())
+    planets = [list(map(int, input().split())) for _ in range(planet_count)]
+
+    for planet in planets:
+        center, radius_squared = planet[0:2], planet[2:]
+
+        start_in = squared_distance(start, center) < radius_squared
+        end_in = squared_distance(end, center) < radius_squared
+
+        if start_in != end_in:
             ans += 1
-        elif between(coordinates[0:2], planets[i][0:2]) < planets[i][2] and between(coordinates[2:], planets[i][0:2]) > planets[i][2]:
-            ans += 1
+
     print(ans)
